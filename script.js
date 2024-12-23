7527f6c23a537ec43ebfdeb154974467f5b51900
stablelane <aaanshad09@gmail.com>
2024-12-23 14:43:10 +0530

    add text inside chatheader and icon for back button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
      // Create floating button
   const floatButton = document.createElement('button');
-  floatButton.textContent = 'Chat';
+  floatButton.textContent = '^';
   floatButton.classList.add('float-button'); 
   document.body.appendChild(floatButton);
 
@@ -21,6 +21,42 @@
     }
   })
 
+  // chat header
+
+  const chatHeader = document.createElement('div');
+  chatHeader.classList.add('chat-header');
+  chatBox.appendChild(chatHeader);
+
+  const minButton = document.createElement('button');
+  const svgIcon = document.createElement('img');
+  svgIcon.src = './arrow.png'; 
+  svgIcon.alt = 'Minimize';
+  minButton.classList.add('min-btn');
+  minButton.appendChild(svgIcon);
+  
+    // Vector images between the minButton and headerText
+  const vector1 = document.createElement('img');
+  vector1.src = './Vector.png'; 
+  vector1.alt = 'Vector ';
+  vector1.classList.add('vector');
+
+  const vector2 = document.createElement('img');
+  vector2.src = './vector1.png'; 
+  vector2.alt = 'Vector 1';
+  vector2.classList.add('vector');
+
+  const vector3 = document.createElement('img');
+  vector3.src = './Vector2.png'; 
+  vector3.alt = 'Vector 2';
+  vector3.classList.add('vector');
+
+  const headerText = document.createElement('span');
+  headerText.textContent = 'Assistant';
+  headerText.classList.add('header-text');
+
+  chatHeader.appendChild(minButton)
+  chatHeader.appendChild(headerText)
+
   // input and button div
 
   const inputDiv = document.createElement('div');
@@ -28,10 +64,14 @@
 
   const chatInput = document.createElement('input');
   chatInput.classList.add('chat-input')
+  chatInput.placeholder = 'Type a reply...'
 
   const chatButton = document.createElement('button')
   chatButton.classList.add('chat-button')
-  chatButton.textContent = 'send';
+  const sendIcon = document.createElement('img');
+  sendIcon.src = './send.png'; 
+  sendIcon.alt = 'send';
+  chatButton.appendChild(sendIcon);
 
   inputDiv.appendChild(chatInput)
   inputDiv.appendChild(chatButton)
