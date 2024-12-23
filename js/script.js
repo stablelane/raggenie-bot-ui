9d0a1ae5887ffc56dbf6fb3a233a5860c2fcdc4e
stablelane <aaanshad09@gmail.com>
2024-12-23 16:23:46 +0530

    format and add comments

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 (function () {
-     // Create floating button
+  populateUI();
+
+
+})();
+
+function populateUI() {
+  // Create floating button
   const floatButton = document.createElement('button');
-  floatButton.classList.add('float-button'); 
+  floatButton.classList.add('float-button');
   document.body.appendChild(floatButton);
 
   const buttonIcon = document.createElement('img');
   buttonIcon.classList.add('button-icon');
   buttonIcon.src = '../assets/arrow.svg';
-  floatButton.appendChild(buttonIcon); 
+  floatButton.appendChild(buttonIcon);
 
   // chatbox display=none(initially)
   const chatBox = document.createElement('div');
   chatBox.classList.add('chat-box');
   chatBox.style.display = 'none';
   document.body.appendChild(chatBox);
-  
 
 
+  // event listener for floatButton
   floatButton.addEventListener('click', () => {
     if (chatBox.style.display === 'block') {
       chatBox.style.display = 'none';
@@ -31,13 +37,21 @@
   chatHeader.classList.add('chat-header');
   chatBox.appendChild(chatHeader);
 
+  // < minimize button
   const minButton = document.createElement('button');
   const arrowIcon = document.createElement('img');
-  arrowIcon.src = '../assets/arrow.svg'; 
+  arrowIcon.src = '../assets/arrow.svg';
   arrowIcon.alt = 'Minimize';
   minButton.classList.add('min-btn');
   minButton.appendChild(arrowIcon);
-  
+  minButton.addEventListener('click', () => {
+    if (chatBox.style.display === 'block') {
+      chatBox.style.display = 'none';
+    } else {
+      chatBox.style.display = 'block';
+    }
+  })
+
   // headerlogo
   const headerLogo = document.createElement('img');
   headerLogo.src = '../assets/logo.svg'
@@ -52,10 +66,11 @@
   chatHeader.appendChild(headerLogo)
   chatHeader.appendChild(headerText)
 
-  // input and button div
+  // input and send button div
 
   const inputDiv = document.createElement('div');
   inputDiv.classList.add('input-div');
+  chatBox.appendChild(inputDiv)
 
   const chatInput = document.createElement('input');
   chatInput.classList.add('chat-input')
@@ -64,14 +79,11 @@
   const chatButton = document.createElement('button')
   chatButton.classList.add('chat-button')
   const sendIcon = document.createElement('img');
-  sendIcon.src = '../assets/send.png'; 
+  sendIcon.src = '../assets/send.png';
   sendIcon.alt = 'send';
   chatButton.appendChild(sendIcon);
 
   inputDiv.appendChild(chatInput)
   inputDiv.appendChild(chatButton)
 
-  chatBox.appendChild(inputDiv)
-
-      
-})();
+}
